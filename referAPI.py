71e4079e37f0259acd1b3a6b0dec62a1acb82c79
Arav <56185632+arav06@users.noreply.github.com>
2023-01-24 12:49:35 +0530

    Create referAPI.py

---
+++ referAPI.py
@@ -0,0 +1,42 @@
+from flask import Flask,request,redirect
+from requests import get as rget
+from json import loads
+from re import match
+from pymongo import MongoClient
+from dotenv import load_dotenv
+load_dotenv()
+
+connection = f"mongodb://mongoadmin:{getenv('mongoPass')}@localhost:27017"
+print(connection)
+
+client = MongoClient(connection)
+app = Flask(__name__)
+
+@app.route("/<address>")
+def redirect():
+  if match("^0x[a-fA-F0-9]{40}$",address):
+    return redirect(f"https://shardeum.app.xade.finance/register/{address}")
+  else:
+    return redirect("https://shardeum.app.xade.finance")
+  
+@app.route("/api/<address>")
+def api():
+  if match("^0x[a-fA-F0-9]{40}$",address):
+    address = address.lower()
+    database = client["shardeum"]
+    users = database["users"]
+    referrals = users.find({"Referral":str(address)})
+    count = 0
+    for ref in referrals:
+      count = count+1
+    return str(count)
+if __name__ == '__main__':
+        app.run('127.0.0.1',8005)
+      
+  
+  
+  
+
+  
+  
+  
