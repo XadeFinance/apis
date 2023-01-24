cb6c502921b9c2880f5cf76f6125d9ff01f0bcbf
Arav <56185632+arav06@users.noreply.github.com>
2023-01-24 13:17:50 +0530

    Update referAPI.py

--- referAPI.py
+++ referAPI.py
@@ -12,25 +12,28 @@ print(connection)
 client = MongoClient(connection)
 app = Flask(__name__)
 
-@app.route("/<address>")
-def redirect(address):
-#   address = str(address)
-#   if match("^0x[a-fA-F0-9]{40}$",address):
-  return address
-#   else:
-#     return redirect("https://shardeum.app.xade.finance")
+# @app.route("/<address>")
+# def redirect(address):
+# #   address = str(address)
+# #   if match("^0x[a-fA-F0-9]{40}$",address):
+#   return address
+# #   else:
+# #     return redirect("https://shardeum.app.xade.finance")
   
-@app.route("/api/<address>")
-def api(address):
-  if match("^0x[a-fA-F0-9]{40}$",address):
-    address = str(address).lower()
-    database = client["shardeum"]
-    users = database["users"]
-    referrals = users.find({"Referral":str(address)})
-    count = 0
-    for ref in referrals:
-      count = count+1
-    return str(count)
+# @app.route("/api/<address>")
+# def api(address):
+#   if match("^0x[a-fA-F0-9]{40}$",address):
+#     address = str(address).lower()
+#     database = client["shardeum"]
+#     users = database["users"]
+#     referrals = users.find({"Referral":str(address)})
+#     count = 0
+#     for ref in referrals:
+#       count = count+1
+#     return str(count)
+@app.route('/<path_param>')
+def return_path_param(path_param):
+    return path_param
 if __name__ == '__main__':
         app.run('127.0.0.1',8005)
       
