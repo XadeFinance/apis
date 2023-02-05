9040e5302b142275a89de6c93bf0a74139cee254
Arav <56185632+arav06@users.noreply.github.com>
2023-02-05 12:29:17 +0530

    Create addressesAPI.py

---
+++ addressesAPI.py
@@ -0,0 +1,25 @@
+from flask import Flask, Response
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+connection = f"mongodb://mongoadmin:{getenv('mongoPass')}@localhost:27017"
+print(connection)
+
+client = MongoClient(connection)
+db = client["shardeum"]
+
+wallets = db["wallets"]
+
+@app.route(f"/{getenv('emailPass')}", methods=["GET"])
+def get_wallets():
+    addresses = []
+    for wallet in wallets.find({}, {"Wallet Address": 1}):
+        addresses.append(wallet["Wallet Address"])
+    return Response("\n".join(addresses), mimetype="text/plain")
+
+if __name__ == "__main__":
+    app.run('127.0.0.1',8009)
