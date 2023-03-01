8299323cc04f749e8304bcad63c65b96ba95bb3f
Arav <56185632+arav06@users.noreply.github.com>
2023-03-01 13:17:09 +0530

    Update mongoAPI.py

--- mongoAPI.py
+++ mongoAPI.py
@@ -1,73 +1,45 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from flask import Flask, request
 from json import loads
 from pymongo import MongoClient
 from os import getenv
 from dotenv import load_dotenv
+
 load_dotenv()
 
 connection = f"mongodb://mongoadmin:{getenv('mongoPass')}@localhost:27017"
 print(connection)
 
 client = MongoClient(connection)
-#client = MongoClient("mongodb://localhost:27017/")
-database = client["xade"]
-users = database["users"]
-wallets = database["wallets"]
-phones = database["phones"]
 
-class mongoAPI(BaseHTTPRequestHandler):
-    def _set_response(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-    def do_GET(self):
-        self.send_response(301)
-        self.send_header('Location','https://app.xade.finance')
-        self.end_headers()
+app = Flask(__name__)
 
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length']) 
-        post_data = self.rfile.read(content_length) 
-        data = post_data.decode('utf-8')
-        #if data.startswith("IP: "):
-        #    i = open("./ips.log",'r')
-        #    ip = open("./ips.log",'a')
-        #    data = data.replace("IP: ","")
-        #    if data not in i.readlines():
-        #       ips.insert_one({"IP Address":data})
-        #        ip.write(data+"\n")
-        #    ip.close()
-        #    i.close()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    database = client["xade"]
+    users = database["users"]
+    wallets = database["wallets"]
+    phones = database["phones"]
+    if request.method == 'GET':
+        return redirect('https://app.xade.finance', code=301)
+    elif request.method == 'POST':
+        data = request.get_data(as_text=True)
         if data.startswith("address:"):
-            # w = open(".\\wallets.log",'r')
-            # addr = open(".\\wallets.log",'a')
             z = data.split("||")
             adr = z[0].replace("address:","")
             i = z[1].replace("id:","")
             addrChk = wallets.find_one({"Wallet Address":adr})
             if addrChk == None:
                 x = wallets.insert_one({"Wallet Address":adr,"ID":i})
-            #     addr.write(adr+" ")
-            # w.close()
-            # addr.close()
 
         elif data.startswith('{"phone":'):
-            # abcd = open(".\\phones.log",'r')
-            # ph = open(".\\phones.log",'a')
             p = loads(data)
             phn = p["phone"]
             i = p["id"]
-            #a = i+'\n'
             phnChk = phones.find_one({"Phone Number":phn})
-            if phnCheck == None:
+            if phnChk == None:
                 x = phones.insert_one({"Phone Number":phn,"ID":i})
-            #     ph.write(phn+" ")
-            # abcd.close()
-            # ph.close()
 
-    
         else:
-            
             j = data.replace("'", "\"")
             d = loads(j)
 
@@ -81,57 +53,133 @@ class mongoAPI(BaseHTTPRequestHandler):
             if login == "jwt":
                 login = "email"
 
-            # info2 = f"{email}\n"
-            # info3 = f"Email: {email} Login Type: {login}\n"
-            # fa = open('.\\emails.log','a')
-            # fr = open('.\\emails.log','r').read().split("\n")
-            # fa2 = open('./logins.log','a')
-            # fr2 = open('./logins.log','r').readlines()
-
             emailAndLoginChk = users.find_one({"Email":email,"Login Type":login})
             if emailAndLoginChk == None:
-    
-    
                 info = {
                     "Email":email,
                     "Username":name,
                     "Login Type":login,
                     "ID":i
                 }
-    
+
                 x = users.insert_one(info)
 
-            # elif info3 not in fr2:
-            #         fa2.write(info3)
-            #         fa2.close()
-    
-            #         info = {
-            #             "Email":email,
-            #             "Username":name,
-            #             "Profile Picture URL":pfp,
-            #             "Login Type":login,
-            #             "IP Address":ip,
-            #             "Primary Login":'No',
-            #             "ID":i
-            #         }
-    
-            #         x = users.insert_one(info)
             else:
                 info = "duplicate lol"
 
-        #print(info)
-        self._set_response()    
-        self.wfile.write("{}".format(self.path).encode('utf-8'))
+        return data
 
-def run(server_class=HTTPServer, handler_class=mongoAPI, port=8000):
-    # logging.basicConfig(level=logging.INFO)
-    server_address = ('127.0.0.1', 8000)
-    httpd = server_class(server_address, handler_class)
-    print(f"Listening on 127.0.0.1:{port}")
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
+@app.route('/shardeum', methods=['GET', 'POST'])
+def shardeum():
+    database = client["shardeum"]
+    users = database["users"]
+    wallets = database["wallets"]
+    phones = database["phones"]
+    if request.method == 'GET':
+        return redirect('https://app.xade.finance', code=301)
+    elif request.method == 'POST':
+        data = request.get_data(as_text=True)
+        if data.startswith("address:"):
+            z = data.split("||")
+            adr = z[0].replace("address:","")
+            i = z[1].replace("id:","")
+            addrChk = wallets.find_one({"Wallet Address":adr})
+            if addrChk == None:
+                x = wallets.insert_one({"Wallet Address":adr,"ID":i})
+
+        elif data.startswith('{"phone":'):
+            p = loads(data)
+            phn = p["phone"]
+            i = p["id"]
+            phnChk = phones.find_one({"Phone Number":phn})
+            if phnChk == None:
+                x = phones.insert_one({"Phone Number":phn,"ID":i})
+
+        else:
+            j = data.replace("'", "\"")
+            d = loads(j)
+
+            email = d["email"]
+            name = d["name"]
+            pfp = d["profileImage"]
+            verify = d["verifier"]
+            i = d["verifierId"]
+            login = d["typeOfLogin"]
+            i = d["id"]
+            if login == "jwt":
+                login = "email"
+
+            emailAndLoginChk = users.find_one({"Email":email,"Login Type":login})
+            if emailAndLoginChk == None:
+                info = {
+                    "Email":email,
+                    "Username":name,
+                    "Login Type":login,
+                    "ID":i
+                }
+
+                x = users.insert_one(info)
+
+            else:
+                info = "duplicate lol"
+
+        return data
+
+@app.route('/polygon', methods=['GET', 'POST'])
+def polygon():
+    database = client["mainnet"]
+    users = database["users"]
+    wallets = database["wallets"]
+    phones = database["phones"]
+    if request.method == 'GET':
+        return redirect('https://app.xade.finance', code=301)
+    elif request.method == 'POST':
+        data = request.get_data(as_text=True)
+        if data.startswith("address:"):
+            z = data.split("||")
+            adr = z[0].replace("address:","")
+            i = z[1].replace("id:","")
+            addrChk = wallets.find_one({"Wallet Address":adr})
+            if addrChk == None:
+                x = wallets.insert_one({"Wallet Address":adr,"ID":i})
+
+        elif data.startswith('{"phone":'):
+            p = loads(data)
+            phn = p["phone"]
+            i = p["id"]
+            phnChk = phones.find_one({"Phone Number":phn})
+            if phnChk == None:
+                x = phones.insert_one({"Phone Number":phn,"ID":i})
+
+        else:
+            j = data.replace("'", "\"")
+            d = loads(j)
+
+            email = d["email"]
+            name = d["name"]
+            pfp = d["profileImage"]
+            verify = d["verifier"]
+            i = d["verifierId"]
+            login = d["typeOfLogin"]
+            i = d["id"]
+            if login == "jwt":
+                login = "email"
+
+            emailAndLoginChk = users.find_one({"Email":email,"Login Type":login})
+            if emailAndLoginChk == None:
+                info = {
+                    "Email":email,
+                    "Username":name,
+                    "Login Type":login,
+                    "ID":i
+                }
+
+                x = users.insert_one(info)
+
+            else:
+                info = "duplicate lol"
+
+        return data
 
 if __name__ == '__main__':
-    run()
+    app.run(host='127.0.0.1', port=8000)
