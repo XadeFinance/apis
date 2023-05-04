05e77c72c9dfd3eb0a8a988c2ab063569dcbc1e6
Arav <56185632+arav06@users.noreply.github.com>
2023-05-05 01:38:31 +0530

    Update userAPI.py

--- userAPI.py
+++ userAPI.py
@@ -18,7 +18,7 @@ def index():
                 return "'address' parameter was not specified",404
         wallet = str(request.args.get("address"))
         #client = MongoClient("mongodb://localhost:27017/")
-        database = client["xade"]
+        database = client["mobile"]
         wallets = database["wallets"]
         try:
                 walletDetails = wallets.find_one({"Wallet Address":str(wallet)})
@@ -36,31 +36,6 @@ def index():
         userDetails = users.find_one({"ID":uid})
         return userDetails["Username"],200
 
-@app.route('/shardeum')
-def shardeum():
-        try:
-                wallet = str(request.args.get("address"))
-        except:
-                return "'address' parameter was not specified",404
-        wallet = str(request.args.get("address"))
-        #client = MongoClient("mongodb://localhost:27017/")
-        database = client["shardeum"]
-        wallets = database["wallets"]
-        try:
-                walletDetails = wallets.find_one({"Wallet Address":str(wallet)})
-        except:
-                return "wallet address was not found",404
-        walletDetails = wallets.find_one({"Wallet Address":str(wallet)})
-        if walletDetails == None:
-            return "wallet address was not found",404
-        uid = walletDetails["ID"]
-        users = database["users"]
-        try:
-                usernameDetails = users.find_one({"ID":uid})
-        except:
-                return "username was not found",404
-        userDetails = users.find_one({"ID":uid})
-        return userDetails["Username"],200
 
 if __name__ == '__main__':
         app.run('127.0.0.1',8003)
