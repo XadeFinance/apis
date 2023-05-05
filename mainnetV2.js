1a5b0acf73e77cf1a6500931bce4da522fa247ab
Arav <56185632+arav06@users.noreply.github.com>
2023-05-05 15:36:09 +0530

    Update mainnetV2.js

--- mainnetV2.js
+++ mainnetV2.js
@@ -7,7 +7,7 @@ const provider = new ethers.JsonRpcProvider(
 );
 let contractAbi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"transferXUSD","outputs":[],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"}]
         const wallet = new ethers.Wallet(
-  process.env.arnavKaaKey,
+  process.env.privateKey,
   provider
 );
 
