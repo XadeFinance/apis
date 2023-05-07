42731ca4444b2ba5ffddaa02fa472dbb8802c821
Arav <56185632+arav06@users.noreply.github.com>
2023-05-07 13:15:35 +0530

    Update mongoAPI.py

--- mongoAPI.py
+++ mongoAPI.py
@@ -4,12 +4,9 @@ from pymongo import MongoClient
 from os import getenv,system
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 connection = f"mongodb://mongoadmin:{getenv('mongoPass')}@localhost:27017"
-print(connection)
-
 client = MongoClient(connection)
 
 app = Flask(__name__)
@@ -71,103 +68,26 @@ def index():
 
         return data
 
-@app.route('/shardeum', methods=['GET', 'POST'])
-def shardeum():
-    database = client["shardeum"]
-    users = database["users"]
-    wallets = database["wallets"]
-    phones = database["phones"]
-    if request.method == 'GET':
-        return redirect('https://app.xade.finance', code=301)
-    elif request.method == 'POST':
-        data = request.get_data(as_text=True)
-        if data.startswith("address:"):
-            z = data.split("||")
-            adr = z[0].replace("address:","")
-            i = z[1].replace("id:","")
-            addrChk = wallets.find_one({"Wallet Address":adr})
-            if addrChk == None:
-                x = wallets.insert_one({"Wallet Address":adr,"ID":i})
-
-        elif data.startswith('{"phone":'):
-            p = loads(data)
-            phn = p["phone"]
-            i = p["id"]
-            phnChk = phones.find_one({"Phone Number":phn})
-            if phnChk == None:
-                x = phones.insert_one({"Phone Number":phn,"ID":i})
-
-        else:
-            j = data.replace("'", "\"")
-            d = loads(j)
-
-            email = d["email"]
-            name = d["name"]
-            pfp = d["profileImage"]
-            verify = d["verifier"]
-            i = d["verifierId"]
-            login = d["typeOfLogin"]
-            i = d["id"]
-            if login == "jwt":
-                login = "email"
-
-            emailAndLoginChk = users.find_one({"Email":email,"Login Type":login})
-            if emailAndLoginChk == None:
-                info = {
-                    "Email":email,
-                    "Username":name,
-                    "Login Type":login,
-                    "ID":i
-                }
-
-                x = users.insert_one(info)
-
-            else:
-                info = "duplicate lol"
-
-        return data
-
 @app.route('/polygon', methods=['GET', 'POST'])
 def polygon():
     database = client["mobile"]
     users = database["users"]
-    wallets = database["wallets"]
-    phones = database["phones"]
     if request.method == 'GET':
         return redirect('https://app.xade.finance', code=301)
     elif request.method == 'POST':
         data = request.get_data(as_text=True)
-        if data.startswith("address:"):
-            z = data.split("||")
-            adr = z[0].replace("address:","")
-            i = z[1].replace("id:","")
-            scw = z[2];
-            addrChk = wallets.find_one({"Wallet Address":adr})
-            if addrChk == None:
-                x = wallets.insert_one({"Wallet Address":adr,"ID":i, "SCW Address":scw})
-            print(adr)
-
-        elif data.startswith('{"phone":'):
-            p = loads(data)
-            phn = p["phone"]
-            i = p["id"]
-            phnChk = phones.find_one({"Phone Number":phn})
-            if phnChk == None:
-                x = phones.insert_one({"Phone Number":phn,"ID":i})
-
+        if data.startswith("test"):
+            return "im up"
         else:
             j = data.replace("'", "\"")
             d = loads(j)
 
             email = d["email"]
             name = d["name"]
-            pfp = d["profileImage"]
-            verify = d["verifier"]
-            i = d["verifierId"]
             login = d["typeOfLogin"]
+            eoa = d["eoa"]
+            scw = d["scw"]
             i = d["id"]
-            if login == "jwt":
-                login = "email"
 
             emailAndLoginChk = users.find_one({"Email":email,"Login Type":login})
             if emailAndLoginChk == None:
@@ -175,6 +95,8 @@ def polygon():
                     "Email":email,
                     "Username":name,
                     "Login Type":login,
+                    "Wallet Address":eoa,
+                    "SCW Address":scw,
                     "ID":i
                 }
                 
@@ -182,29 +104,23 @@ def polygon():
                 testnet = v2Db["testnet"]
                 mainnet = v2Db["mainnet"]
 
+                x = users.insert_one(info)
+                address = scw
                 checkTestnet = testnet.find_one({'Email': email})
                 if checkTestnet:
                     amount = int(float(checkTestnet.get("Amount")) * pow(10,18))
-                    checkAddress = wallets.find_one({'ID':i})
-                    if checkAddress:
-                        address = checkAddress.get("SCW Address")
-                        print(address)
-                        print(amount)
-                        system(f"node /home/xade/xade-api/testnetV2.js {address} {amount}") 
-                        deleteDoc = testnet.delete_one({'Email': email})
+                    print(address)
+                    print(amount)
+                    system(f"node /home/xade/xade-api/testnetV2.js {address} {amount}") 
+                    deleteDoc = testnet.delete_one({'Email': email})
                 
                 checkMainnet = mainnet.find_one({'Email': email})
                 if checkMainnet:
                     amount = int(float(checkMainnet.get("Amount")) * pow(10,6))
-                    checkAddress = wallets.find_one({'ID':i})
-                    if checkAddress:
-                        address = checkAddress.get("SCW Address")
-                        print(address)
-                        print(amount)
-                        system(f"node /home/xade/xade-api/mainnetV2.js {address} {amount}")
-                        deleteDoc = mainnet.delete_one({'Email': email})
-
-                x = users.insert_one(info)
+                    print(address)
+                    print(amount)
+                    system(f"node /home/xade/xade-api/mainnetV2.js {address} {amount}")
+                    deleteDoc = mainnet.delete_one({'Email': email})
                 
                 print(email)
             else:
@@ -213,4 +129,4 @@ def polygon():
         return data
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
+    app.run(host='127.0.0.1', port=8000
