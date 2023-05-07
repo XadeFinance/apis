fbf9d10bae359114ab793e1ce4f1984ef18e1fe4
Arav <56185632+arav06@users.noreply.github.com>
2023-05-07 13:17:33 +0530

    Update emailPaymentsAPI.py

--- emailPaymentsAPI.py
+++ emailPaymentsAPI.py
@@ -2,11 +2,9 @@ from pymongo import MongoClient
 from flask import Flask,request,abort
 from os import getenv
 from dotenv import load_dotenv
-
 load_dotenv()
 
 connection = f"mongodb://mongoadmin:{getenv('mongoPass')}@localhost:27017"
-print(connection)
 
 client = MongoClient(connection)
 app = Flask(__name__)
@@ -38,32 +36,6 @@ def index():
 
         return walletDetails["Wallet Address"],200
 
-@app.route('/shardeum')
-def shardeum():
-        try:
-                email = str(request.args.get("email"))
-        except:
-                return "'email' parameter was not specified",404
-        email = str(request.args.get("email"))
-        #client = MongoClient("mongodb://localhost:27017/")
-        database = client["shardeum"]
-        users = database["users"]
-        try:
-                userDetails = users.find_one({"Email":str(email)})
-        except:
-                return f"Email Address was not found",404
-        userDetails = users.find_one({"Email":str(email)})
-        if userDetails == None:
-            return "Email Address was not found",404
-        uid = userDetails["ID"]
-        wallets = database["wallets"]
-        try:
-                walletDetails = wallets.find_one({"ID":uid})
-        except:
-                return "Wallet Address was not found",404
-        walletDetails = wallets.find_one({"ID":uid})
-        return walletDetails["Wallet Address"],200
-
 @app.route('/polygon')
 def polygon():
         try:
@@ -71,7 +43,6 @@ def polygon():
         except:
                 return "'email' parameter was not specified",404
         email = str(request.args.get("email"))
-        #client = MongoClient("mongodb://localhost:27017/")
         database = client["mobile"]
         users = database["users"]
         try:
@@ -81,40 +52,8 @@ def polygon():
         userDetails = users.find_one({"Email":str(email)})
         if userDetails == None:
             return "Email Address was not found",404
-        uid = userDetails["ID"]
-        wallets = database["wallets"]
-        try:
-                walletDetails = wallets.find_one({"ID":uid})
-        except:
-                return "Wallet Address was not found",404
-        walletDetails = wallets.find_one({"ID":uid})
-        return walletDetails["Wallet Address"],200
-
-@app.route('/polygonscw')
-def polygonScw():
-        try:
-                email = str(request.args.get("email"))
-        except:
-                return "'email' parameter was not specified",404
-        email = str(request.args.get("email"))
-        #client = MongoClient("mongodb://localhost:27017/")
-        database = client["mobile"]
-        users = database["users"]
-        try:
-                userDetails = users.find_one({"Email":str(email)})
-        except:
-                return f"Email Address was not found",404
-        userDetails = users.find_one({"Email":str(email)})
-        if userDetails == None:
-            return "Email Address was not found",404
-        uid = userDetails["ID"]
-        wallets = database["wallets"]
-        try:
-                walletDetails = wallets.find_one({"ID":uid})
-        except:
-                return "Wallet Address was not found",404
-        walletDetails = wallets.find_one({"ID":uid})
-        return walletDetails["SCW Address"],200
+        address = userDetails["SCW Address"]
+        return address,200
 
 @app.route('/polygonlogintype')
 def polygonLogintype():
@@ -123,7 +62,6 @@ def polygonLogintype():
         except:
                 return "'email' parameter was not specified",404
         email = str(request.args.get("email"))
-        #client = MongoClient("mongodb://localhost:27017/")
         database = client["mobile"]
         users = database["users"]
         try:
