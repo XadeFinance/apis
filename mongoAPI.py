2f3f5c7cb610bce581bafd3ce61ced718d719e0f
Arnav-Jhajharia <jhajhariaarnav@gmail.com>
2023-07-24 18:36:07 +0530

    Update mongoAPI.py

--- mongoAPI.py
+++ mongoAPI.py
@@ -156,6 +156,12 @@ def polygon():
                 info = "duplicate lol"
 
         return data
-
+    def blueprintig():
+        database = client["extra-info"]
+        extras = database["extras"]
+        return "hi", 200
+        
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000)
+
+
